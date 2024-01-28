b584b134de02a88ea439bedab63f42cbb7373718
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-01-27 19:34:41 -0500

    Initial Commit

---
+++ CoinGeckoIOS/CoinGeckoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoIOSApp.swift
+//  CoinGeckoIOS
+//
+//  Created by Teodoro Calle Lara on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
